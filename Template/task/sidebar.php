12da3655ebb297dfe616b9abdd9567110de236c4
Franklin Hernandez <hernandezfo@hotmail.com>
2019-01-16 11:10:18 -0500

    Links Fixed.
    Wrong links were defined within sidebar.php. Right ones were added and wrong one was deleted.

--- Template/task/sidebar.php
+++ Template/task/sidebar.php
@@ -1,6 +1,9 @@
 <?php if ($this->user->hasProjectAccess('TaskModificationController', 'edit', $task['project_id'])): ?>
 <?php else: ?>
         <li>
-            <?= $this->modal->small('comment-o', t('Add a comment'), 'CommentController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+            <?= $this->modal->medium('file', t('Attach a document'), 'TaskFileController', 'create', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
+        </li>
+        <li>
+            <?= $this->modal->medium('camera', t('Add a screenshot'), 'TaskFileController', 'screenshot', array('task_id' => $task['id'], 'project_id' => $task['project_id'])) ?>
         </li>
 <?php endif ?>
